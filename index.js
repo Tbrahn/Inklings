fc597c2e199b1baccc1d9cb32614cc9640cf9d16
Tbrahn <tbranham9323@gmail.com>
2019-02-14 09:13:43 -0500

    change logo size

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(window).scroll(function(){
     var scroll = $(window).scrollTop();
-    if(scroll < 100){
+    if(scroll < 85){
         $('.navbar').css('background', 'transparent');
     } else{
         $('.navbar').css('background', 'rgba(255, 255, 255, 0.9)');
