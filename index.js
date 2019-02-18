6fff273f45a35df6309de3ec154806e3b50138f0
Tbrahn <tbranham9323@gmail.com>
2019-02-18 14:53:20 -0500

    add hide navbar on scroll for mobile devices

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ $(window).scroll(function(){
         $('.navbar').css('background', 'rgba(255, 255, 255, 0.9)');
     }
 });
+
+
+if(window.screen.width < 500){
+    var lastScrollTop = 0;
+
+    window.addEventListener("scroll", function(){  
+    var st = window.pageYOffset || document.documentElement.scrollTop;  
+    if (st > lastScrollTop){
+        document.getElementById("navbar").style.top = "100%";
+    } else {
+        document.getElementById("navbar").style.top = "0";
+    }
+    lastScrollTop = st;
+    }, false);
+}
