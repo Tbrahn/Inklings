b02e6b3d4677754d03fd2c77f1138394e2d9fe07
Tbrahn <tbranham9323@gmail.com>
2019-02-21 09:27:18 -0500

    change colors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-$(window).scroll(function(){
-    var scroll = $(window).scrollTop();
-    if(scroll < 85){
-        $('.navbar').css('background', 'transparent');
-    } else{
-        $('.navbar').css('background', 'rgba(255, 255, 255, 0.9)');
-    }
-});
+// $(window).scroll(function(){
+//     var scroll = $(window).scrollTop();
+//     if(scroll < 85){
+//         $('.navbar').css('background', 'transparent');
+//     } else{
+//         $('.navbar').css('background', 'rgba(255, 255, 255, 0.9)');
+//     }
+// });
